ccf91128b5085c95b2fac1a4600d01c03909142c
Chambi-Dev <a3820143@gmail.com>
2026-08-26 16:32:41 -0500

    change host

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { db } from '@/db';
 import { usuarios, accounts, sessions, verificationTokens } from '@/db/schema';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   adapter: DrizzleAdapter(db, {
     usersTable: usuarios,
     accountsTable: accounts,
